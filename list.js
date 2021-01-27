e8c9046eb88f8acc5bb14171d15beb0a2f7b46b0
facundo <lotofacundo6@gmail.com>
2021-01-27 04:21:49 -0300

    pokedex

--- list.js
+++ list.js
@@ -121,77 +121,77 @@ function colorCard(id,bg,type){
   case "normal":
  
   id.className="card-img-top  normal"
-  type.src="/img/normal.png" //en base al tipo se le da un fondo y un sello en type del template
+  type.src="./img/normal.png" //en base al tipo se le da un fondo y un sello en type del template
   break;
   case "water":
     
   id.className=" card-img-top  water"
 
-  type.src="/img/water.png"
+  type.src="./img/water.png"
   break;  
   case "electric":
   id.className="card-img-top electric"
-  type.src="/img/electric.png"
+  type.src="./img/electric.png"
   break;
   case "fighting":
   id.className="card-img-top  fighting"
-  type.src="/img/fighting.png"
+  type.src="./img/fighting.png"
   break;
   case "ground":
   id.className=" card-img-top  ground"
-  type.src="/img/ground.png"
+  type.src="./img/ground.png"
   break;
   case "psychic":
   id.className=" card-img-top psychic"
-  type.src="/img/psychic.png"
+  type.src="./img/psychic.png"
   break;
   case "rock":
   id.className=" card-img-top rock"
-  type.src="/img/rock.png"
+  type.src="./img/rock.png"
   break;
   case "dark":
   id.className=" card-img-top dark"
-  type.src="/img/dark.png"
+  type.src="./img/dark.png"
   break;
   case "steel":
   id.className=" card-img-top steel"
-  type.src="/img/steel.png"
+  type.src="./img/steel.png"
   break;
   case "grass":
   id.className=" card-img-top grass"
-  type.src="/img/grass.png"
+  type.src="./img/grass.png"
   break;
   case "ice":
   id.className=" card-img-top ice"
-  type.src="/img/ice.png"
+  type.src="./img/ice.png"
   break;
   case "poison":
   id.className=" card-img-top poison"
-  type.src="/img/poison.png"
+  type.src="./img/poison.png"
   break;
   case "bug":
   id.className=" card-img-top bug"
-  type.src="/img/bug.png"
+  type.src="./img/bug.png"
   break;
   case "ghost":
   id.className=" card-img-top ghost"
-  type.src="/img/ghost.png"
+  type.src="./img/ghost.png"
   break;
   case "dragon":
   id.className=" card-img-top dragon"
-  type.src="/img/dragon.png"
+  type.src="./img/dragon.png"
   break;
   case "fairy":
   id.className=" card-img-top fairy"
-  type.src="/img/fairy.png"
+  type.src="./img/fairy.png"
   break;
   case "fire":
   id.className=" card-img-top  fire"
-  type.src="/img/fire.png"
+  type.src="./img/fire.png"
   break;
   case "flying":
   id.className=" card-img-top flying"
-  type.src="/img/flying.png"
+  type.src="./img/flying.png"
   break;
   }
 }
