4bbff4ffa250ffcfb5193b437ebd6502e07b6f11
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2022-01-03 02:05:13 -0300

    Delete pokedex-main directory

--- pokedex-main/ajax.js
+++
@@ -1,9 +0,0 @@
-async function allPokemon(){
-    let result_pokemon=[]
-    for(let i=min_poke;i<max_poke;i++){
-        let total=await fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`) //los await sirve para que se vuelva sincrono,osea que lo que sigue despuesde await espere hasta que termine de ejecutar
-        let pokemon=  await total.json()
-        result_pokemon.push(pokemon)
-        console.log("cargando")
-        }
-}
